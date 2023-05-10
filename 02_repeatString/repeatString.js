3cfd0803d9da07de8b96780dee12cfe1dafbbed1
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-05-09 20:53:51 -0700

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, numOfRepeats) {
+	if (numOfRepeats < 0) return 'ERROR';
+	let output = '';
+	for (let i = 0; i < numOfRepeats; i++) {
+		output += str;
+	}
+	return output;
 };
 
 // Do not edit below this line
