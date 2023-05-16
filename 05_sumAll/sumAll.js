37657e765e4e16d6126b5fd5599c0437657570ef
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-05-15 22:03:01 -0700

    complete js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+	if (typeof numOne !== 'number' || typeof numTwo !== 'number' || numOne < 0 || numTwo < 0)
+		return 'ERROR';
+	const min = numOne < numTwo ? numOne : numTwo;
+	const max = numOne > numTwo ? numOne : numTwo;
+	let count = min;
+	let sum = 0;
+	while (count <= max) {
+		sum += count;
+		count++;
+	}
+	return sum;
 };
 
 // Do not edit below this line
