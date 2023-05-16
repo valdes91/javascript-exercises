9771cde4dea93b6e4c8d9748fd1ff0f8a2d237b3
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-05-15 21:53:50 -0700

    complete js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+	return arr.filter((element) => {
+		if (!args.includes(element)) return element;
+	});
 };
 
 // Do not edit below this line
