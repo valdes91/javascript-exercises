d390ede44e75ff2d033fff7175c0a749e6f40619
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-06-11 15:11:25 -0700

    complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+	let total = 0;
+	numbers.forEach((num) => {
+		total += num;
+	});
+
+	return total;
 };
 
-const multiply = function() {
+const multiply = function (numbers) {
+	if (numbers.includes(0)) return 0;
 
+	let product = 1;
+	numbers.forEach((num) => {
+		product *= num;
+	});
+
+	return product;
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	if (num === 0) return 1;
+	else return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
