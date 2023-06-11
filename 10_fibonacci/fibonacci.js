3f81042700e6299a8666e3dde601f114e010f422
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-06-11 16:17:08 -0700

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+	let integer = parseInt(num);
+	if (integer <= 0) return 'OOPS';
+	if (integer <= 2) return 1;
 
+	const fibonacci = [1, 1];
+	let counter = 3;
+	let fib;
+	while (counter <= integer) {
+		fib = fibonacci[0] + fibonacci[1];
+		if (counter % 2 === 0) fibonacci[0] = fib;
+		else fibonacci[1] = fib;
+		counter++;
+	}
+
+	return fib;
 };
 
 // Do not edit below this line
