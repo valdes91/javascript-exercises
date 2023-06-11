43bce9df8c0ec6bbe1f7c585bbd643999efb256d
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-06-11 15:55:09 -0700

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const lower = str.toLowerCase().replace(' ', '');
 
+	//the regex below filters out all non alphanumeric characters and underscores
+	const validCharacters = lower.replace(/\W|_/g, '');
+
+	const reversed = [...validCharacters].reverse().join('');
+
+	return validCharacters === reversed;
 };
 
 // Do not edit below this line
