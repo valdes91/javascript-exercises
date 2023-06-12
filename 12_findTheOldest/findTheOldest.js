a5516581e276e360647bbd28c55efcc0a24ff115
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-06-12 15:58:51 -0700

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	const oldest = people.reduce((oldestPerson, person) => {
+		const thisYear = new Date().getFullYear();
+		const personAge = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+		const oldestPersonAge = (oldestPerson.yearOfDeath || thisYear) - oldestPerson.yearOfBirth;
 
+		return personAge > oldestPersonAge ? person : oldestPerson;
+	}, people[0]);
+
+	return oldest;
 };
 
 // Do not edit below this line
